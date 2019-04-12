ae9a2a64628f9477029b91da8ad80decd0018751
mbw3641193 <305683054@qq.com>
2019-04-12 12:58:33 +0800

    新增 all方法 。Promise A+完结

--- promise-A+.js
+++ promise-A+.js
@@ -4,19 +4,19 @@ class Promise {
 
         this.status = 'pending';
         this.value = undefined;
-        this.fulfilledContainer = [];   //存成功要执行的方法
-        this.rejectedContainer = [];    //存失败要执行的方法
+        this.fulfilledContainer = [];               //存成功要执行的方法
+        this.rejectedContainer = [];                //存失败要执行的方法
 
         //执行excutorCB (增加异常捕获)
         //resolve   代表成功
         let resolve = (result) => {
-            let timer = setTimeout(() => {  //加入定时器使方法变为异步，这样就可以在then执行之后再执行
+        let timer = setTimeout(() => {              //加入定时器使方法变为异步，这样就可以在then执行之后再执行
                 clearTimeout(timer); //销毁定时器
                 if (this.status !== 'pending') return;
                 this.status = 'fulfilled';
                 this.value = result;
                 this.fulfilledContainer.forEach(item => {
-                    item(this.value);  //遍历then中的方法，并把result传给then
+                    item(this.value);               //遍历then中的方法，并把result传给then
                 })
             }, 0);
 
@@ -24,13 +24,13 @@ class Promise {
 
         //reject   代表失败
         let reject = (reason) => {
-            let timer = setTimeout(() => {  //加入定时器使方法变为异步，这样就可以在then执行之后再执行
+            let timer = setTimeout(() => {          //加入定时器使方法变为异步，这样就可以在then执行之后再执行
                 clearTimeout(timer); //销毁定时器
                 if (this.status !== 'pending') return;
                 this.status = 'rejected';
                 this.value = reason;
                 this.rejectedContainer.forEach(item => {
-                    item(this.value);  //遍历then中的方法，并把reason传给then
+                    item(this.value);               //遍历then中的方法，并把reason传给then
                 })
             }, 0);
         };
@@ -47,26 +47,26 @@ class Promise {
     }
 
     then(fulfilledCB, rejectedCB) {
-        if( typeof fulfilledCB !== 'function' ){ //如果不传成功的回调
-            fulfilledCB = result => result      //简写，相当于 result => { return result }
+        if (typeof fulfilledCB !== 'function') {    //如果不传成功的回调
+            fulfilledCB = result => result          //简写，相当于 result => { return result }
         }
-        if( typeof rejectedCB !== 'function' ){ //如果不传错误的回调
+        if (typeof rejectedCB !== 'function') {     //如果不传错误的回调
             rejectedCB = reason => {
-                throw new Error(reason.message);  //thorw不支持简写
+                throw new Error(reason instanceof Error ? reason.message : reason);  //thorw不支持简写
             }
         }
-        return new Promise((resolve, reject) => {  //实现then的链式写法
+        return new Promise((resolve, reject) => {   //实现then的链式写法
             this.fulfilledContainer.push((val) => {
                 try {
                     let x = fulfilledCB(val);
                     // console.log(val === this.value);
 
-                    if (x instanceof Promise) {   //检查x是不是Promise实例
-                        x.then(resolve, reject); //如果是Promise的实例，那么Promise实例本身存在成功与失败，成功走resolve，失败走reject
+                    if (x instanceof Promise) {     //检查x是不是Promise实例
+                        x.then(resolve, reject);    //如果是Promise的实例，那么Promise实例本身存在成功与失败，成功走resolve，失败走reject
                         return;
                     }
-                    
-                    resolve(x);                 //如果不是Promise实例，直接返回值就可以了，下个then直接就接收这个值，不存在失败
+
+                    resolve(x);                     //如果不是Promise实例，直接返回值就可以了，下个then直接就接收这个值，不存在失败
                 } catch (err) {
                     reject(err);
                 }
@@ -76,12 +76,12 @@ class Promise {
                 try {
                     let x = rejectedCB(val);
 
-                    if (x instanceof Promise) {   //检查x是不是Promise实例
-                        x.then(resolve, reject); //如果是Promise的实例，那么Promise实例本身存在成功与失败，成功走resolve，失败走reject
+                    if (x instanceof Promise) {     //检查x是不是Promise实例
+                        x.then(resolve, reject);    //如果是Promise的实例，那么Promise实例本身存在成功与失败，成功走resolve，失败走reject
                         return;
                     }
 
-                    resolve(x);                 //如果不是Promise实例，直接返回值就可以了，下个then直接就接收这个值，不存在失败
+                    resolve(x);                     //如果不是Promise实例，直接返回值就可以了，下个then直接就接收这个值，不存在失败
                 } catch (err) {
                     reject(err);
                 }
@@ -93,8 +93,26 @@ class Promise {
         // this.rejectedContainer.push(rejectedCB);
     }
 
-    catch(rejectedCB){   //catch相当于then方法第一个参数不传，只穿第二个
-        return this.then(null,rejectedCB);
+    catch(rejectedCB) {   //catch相当于then方法第一个参数不传，只穿第二个
+        return this.then(null, rejectedCB);
+    }
+
+    static all(promiseArr = []) {   //static:把Promise当做对象添加私有属性，用Promise.all来执行
+        return new Promise((resolve, reject) => {
+
+            let index = 0,              //记录成功几个
+                resultArr = [];            //把成功的结果放入result中
+            promiseArr.forEach((item, value) => {
+                item.then((result) => {
+                    index++;
+                    resultArr[value] = result;
+                    if (index === promiseArr.length) { //当index与 promiseArr.length 长度相等的时候
+                        resolve(resultArr);
+                    }
+                }, reject);
+
+            });
+        })
     }
 }
 
--- promise-test.js
+++ promise-test.js
@@ -1,21 +1,58 @@
 //用来测试自己写的Promise实例
 let Promise = require('./promise-A+.js');
 
-new Promise((resolve, reject) => {
-    // throw new Error('123');
-    resolve(100);
-    reject(200);
+// new Promise((resolve, reject) => {
+//     // throw new Error('123');
+//     resolve(100);
+//     reject(200);
 
-}).then((result)=>{
-    console.log(1);
-    console.log(result);
-    return result;
-},() => {
-    console.log(2);
-}).then((result) => {
-    console.log(4);
-    console.log(result);
-}, () => {
-    console.log(5);
+// }).then((result)=>{
+//     console.log(1);
+//     console.log(result);
+//     return result;
+// },() => {
+//     console.log(2);
+// }).then((result) => {
+//     console.log(4);
+//     console.log(result);
+// }, () => {
+//     console.log(5);
+// })
+// console.log(3);
+
+let p1 = new Promise((resolve,reject)=>{
+    setTimeout(() => {
+        resolve(100);
+    }, 50);
 })
-console.log(3);
+
+let p2 = new Promise((resolve,reject)=>{
+    setTimeout(() => {
+        resolve(200);
+    }, 10);
+})
+
+let p3 = new Promise((resolve,reject)=>{
+    setTimeout(() => {
+        resolve(300);
+    }, 100);
+})
+
+Promise.all([p1,p2,p3]).then(result=>{
+    /** 
+     * 实现效果：
+     * 所有Promise都成功执行
+     * result中分别存储每个实例返回的结果，而且和数组的顺序是一样的
+    */
+
+    console.log(result);
+}).catch(reason=>{
+    /** 
+     * 实现效果：
+     * 只要有一个失败，就执行catch
+     * 失败后不再执行后面的操作
+    */
+
+    console.log('reason'+reason);
+    
+})
